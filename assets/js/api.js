4b47c870fd09e76e1ef9e82951efebfe97765646
Elias <elias.nau@outlook.de>
2023-08-06 14:57:52 +0200

    Add files via upload

--- assets/js/api.js
+++ assets/js/api.js
@@ -21,15 +21,15 @@ export const url = {
     return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
   },
   airPollution(lat, lon) {
-    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
   },
   reverseGeo(lat, lon) {
-    return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
   },
   /**
    * @param {string} query Search query e.g.: "London", "New York"
    */
   geo(query) {
-    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
   }
 }
